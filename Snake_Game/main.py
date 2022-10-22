ebf2742484ad149732a12746ed2fda6394b94394
Nika-Chinchaladze <106172218+Nika-Chinchaladze@users.noreply.github.com>
2022-10-22 18:43:46 +0400

    improvement

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -12,32 +12,45 @@ screen.bgcolor("dark sea green")
 screen.title("Snake Game")
 screen.tracer(0)
 
-# create snake and random food:
-snake = Snake()
+# create random food:
 food = Food()
 score = ScoreBoard()
 
-screen.listen()
-screen.onkey(snake.up, "w")
-screen.onkey(snake.down, "s")
-screen.onkey(snake.left, "a")
-screen.onkey(snake.right, "d")
+play_again = True
+while play_again:
+    snake = Snake()
+    screen.listen()
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.down, "s")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.right, "d")
 
-play = True
-while play:
-    screen.update()
-    sleep(0.1)
-    snake.move()
-    if (snake.snake_head.xcor() > 285 or snake.snake_head.xcor() < -290) or \
-            (snake.snake_head.ycor() > 290 or snake.snake_head.ycor() < -285):
-        score.game_over()
-        play = False
-    if snake.crashed_snake():
-        score.game_over()
-        play = False
-    if snake.snake_head.distance(food) < 15:
-        food.change_position()
+    play = True
+    while play:
+        screen.update()
+        sleep(0.1)
+        snake.move()
+        if (snake.snake_head.xcor() > 285 or snake.snake_head.xcor() < -290) or \
+                (snake.snake_head.ycor() > 290 or snake.snake_head.ycor() < -285):
+            score.game_over()
+            play = False
+        if snake.crashed_snake():
+            score.game_over()
+            play = False
+        if snake.snake_head.distance(food) < 15:
+            food.change_position()
+            score.write_score()
+            snake.increase_snake()
+
+    will = screen.textinput(title="continue or not", prompt="Do You Want To Continue?").lower()
+    if will == "no":
+        play_again = False
+    else:
+        score.clear()
+        score.current_score = -1
         score.write_score()
-        snake.increase_snake()
+        food.clear()
+        snake.reset_snake()
 
+        
 screen.exitonclick()
